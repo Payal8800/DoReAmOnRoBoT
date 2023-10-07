1ee97fbb348962ccacb4ace59e4f02f4a3fa411e
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2023-10-07 19:44:25 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,10 +7,10 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5655112788 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 6539720313 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "INCRICIBLE"  # Your own group for support, do not add the @
-    START_IMG = ""
+    START_IMG = "https://te.legra.ph/file/f358d4812dfb42931316b.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
     # RECOMMENDED
@@ -22,10 +22,10 @@ class Config(object):
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = [5655112788]  # User id of sudo users
-    DEV_USERS = [5655112788]  # User id of dev users
-    DEMONS = [5655112788]  # User id of support users
-    TIGERS = [5655112788]  # User id of tiger users
+    DRAGONS = [6539720313]  # User id of sudo users
+    DEV_USERS = [6539720313]  # User id of dev users
+    DEMONS = [6539720313]  # User id of support users
+    TIGERS = [6539720313]  # User id of tiger users
     WOLVES = []  # User id of whitelist users
 
     ALLOW_CHATS = True
